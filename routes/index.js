efda5ff17e94d041a102feccad1fd6a51f096924
Gabriel <arpad.g.bondor@gmail.com>
2021-01-08 12:57:25 +0000

    add user and index routes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,55 @@ router.get('/', (req, res, next) => {
   res.render('index', { title: 'Surf Shop - Home' });
 });
 
+/* GET register */
+router.get('/register', (req, res, next) => {
+  res.send('GET /register');
+});
+
+/* POST register */
+router.post('/register', (req, res, next) => {
+  res.send('POST /register');
+});
+
+/* GET login */
+router.get('/login', (req, res, next) => {
+  res.send('GET /login');
+});
+
+/* POST login */
+router.post('/login', (req, res, next) => {
+  res.send('POST /login');
+});
+
+/* GET profile */
+router.get('/profile', (req, res, next) => {
+  res.send('GET /profile');
+});
+
+/* PUT update profile */
+router.put('/profile/:user_id', (req, res, next) => {
+  res.send('UPDATE /profile/:user_id');
+});
+
+/* GET forgot password*/
+router.get('/forgot', (req, res, next) => {
+  res.send('GET /forgot');
+});
+
+/* PUT forgot password */
+router.put('/forgot', (req, res, next) => {
+  res.send('PUT /forgot');
+});
+
+/* GET reset password */
+router.get('/reset/:token', (req, res, next) => {
+  res.send('GET /reset/:token');
+});
+
+/* PUT reset password */
+router.put('/reset/:token', (req, res, next) => {
+  res.send('PUT /reset/:token');
+});
+
+
 module.exports = router;
